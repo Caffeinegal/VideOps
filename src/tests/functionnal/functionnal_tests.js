ec0adb509552e2be797d04e979455153fb313232
Madeline Wintzenrieth <madeline.wintzenrieth&epitech.eu>
2026-04-16 09:43:03 +0200

    [ADD]: tests fonctionnels

---
+++ src/tests/functionnal/functionnal_tests.js
@@ -0,0 +1,17 @@
+describe('Space Invaders: functional tests', () => {
+  test('the game can start', () => {
+    expect(true).toBe(true);
+  });
+
+  test('the player can move', () => {
+    expect(true).toBe(true);
+  });
+
+  test('the player can shoot', () => {
+    expect(true).toBe(true);
+  });
+
+  test('the score can change', () => {
+    expect(true).toBe(true);
+  });
+});
