be304598255cbf1306d65cc58ff75f3c1bdd29ab
Brandon Picq <brandon.picq@epitech.eu>
2026-04-17 11:00:48 +0200

    [FIX]: Fix multiple configs to the test suite that wouldn't import correctly

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src/tests"],
-  testMatch: ["**/functionnal_tests.js", "**/*.test.js", "**/*.spec.js"],
+  testMatch: ["**/functionnal_tests.js", "**/*.test.js", "**/*_tests.js", "**/*.spec.js"],
   moduleFileExtensions: ["js", "ts", "json"],
 };
