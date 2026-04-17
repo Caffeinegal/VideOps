b106b98bd3fa0aef74ddea857426d05c372239a4
Madeline Wintzenrieth <madeline.wintzenrieth&epitech.eu>
2026-04-17 08:58:04 +0200

    [FIX]: file name fix on jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  testMatch: ['**/*.js'],
+  testMatch: ['**/functionnal_tests.js', '**/*.test.js', '**/*.spec.js'],
   moduleFileExtensions: ['js', 'ts', 'json']
 };
